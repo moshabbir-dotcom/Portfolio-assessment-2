9aef668d11e8c747f14a509bd89c6b80abfe60d6
moshabbir-dotcom <moshabbir@ymail.com>
2021-08-31 15:21:11 +0000

    add spinCard if/else statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,22 @@
 const cards = document.querySelectorAll(".playing-card");
 
+let hasTurnedCard = false;
+let card1, card2;
+
 function spinCard() {
-    this.classList.toggle("spin");
+    this.classList.add("spin");
+
+    if (!hasTurnedCard) {
+        //this is the first spin
+        hasTurnedCard = true;
+        card1 = this;
+    }   else {
+        //this is the second spin
+        hasTurnedCard = false;
+        card2 = this;
+
+        //this is to check if both cards match
+    }
 }
 
 cards.forEach(card => card.addEventListener("click", spinCard))
