4ad215809783261d2ce3544994e878018c70ab31
moshabbir-dotcom <moshabbir@ymail.com>
2021-08-31 14:36:47 +0000

    add spinCard function

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const cards = document.querySelectorAll(".playing-card");
+
+function spinCard(){
+    this.classList.toggle("spin");
+}
+
+cards.forEach(card => card.addEventListener("click", spinCard))
