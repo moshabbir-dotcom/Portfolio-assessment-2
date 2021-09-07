2251dcc3d4dd87409c174f37f33d47be6d723395
moshabbir-dotcom <moshabbir@ymail.com>
2021-09-07 12:20:18 +0000

    add stop function for stopwatch

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,5 +73,17 @@ function timeStart(){
     clearInterval(timer);
     timer = setInterval(() => {
       millisecound += 10;
-    }
+      let dateTimer = new Date(millisecound);
+
+      watch.innerHTML = 
+      ('0'+dateTimer.getUTCHours()).slice(-2) + ':' +
+      ('0'+dateTimer.getUTCMinutes()).slice(-2) + ':' +
+      ('0'+dateTimer.getUTCSeconds()).slice(-2) + ':' +
+      ('0'+dateTimer.getUTCMilliseconds()).slice(-3,-1);
+    }, 10);
 }
+
+function timePaused() {
+    watch.style.color = "red";
+    clearInterval(timer);
+  }
